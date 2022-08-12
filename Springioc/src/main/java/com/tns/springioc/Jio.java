1e13a93355cf2224ebf924749bd17c52550ed489
Darshan0707 <darshanc@sjcem.edu.in>
2022-08-12 19:11:40 +0530

    1st commit

---
+++ Springioc/src/main/java/com/tns/springioc/Jio.java
@@ -0,0 +1,19 @@
+package com.tns.springioc;
+
+public class Jio implements Sim {
+	public Jio()
+	{
+		System.out.println("JIO constructor is called");	
+	}
+
+	public void calling() {
+		// TODO Auto-generated method stub
+		System.out.println("calling using jio sim");
+	}
+
+	public void data() {
+		// TODO Auto-generated method stub
+		System.out.println("Browsing using jio sim");
+	}
+
+}
